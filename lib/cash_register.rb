d0a1d5af52b6a91085c745f257315e2ca7d4dae8
Don Ward <10is0420@gmail.com>
2019-08-03 11:35:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
-
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :items, :discount
+  
+  def initialize(total = 0, discount = 20)
+    @total = total
+    items = []
+    self.discount = discount
+  end
+  
+  def add_item(title, price, quantity=0)
+    self.total += price * quantity
+    quantity.times do
+      self.items << item
+    end
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+    
+  
+end
